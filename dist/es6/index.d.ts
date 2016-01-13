4ca5e4a395629152812bb1da116959dee9e54bd0
Niels Vleeming <niels.vleeming@servicebureau.nl>
2016-01-13 17:50:26 +0100

    Another attempt to get a module in the output

--- dist/es6/index.d.ts
+++ dist/es6/index.d.ts
@@ -1,3 +1,8 @@
 import { FrameworkConfiguration } from 'aurelia-framework';
 import { AureliaAdalConfig } from './aurelia-adal-config';
-export declare function configure(frameworkConfig: FrameworkConfiguration, config: AureliaAdalConfig): void;
+import { AureliaAdalManager } from './aurelia-adal-manager';
+export declare module AureliaAdal {
+    function configure(frameworkConfig: FrameworkConfiguration, config: AureliaAdalConfig): void;
+    class AureliaAdalManager2 extends AureliaAdalManager {
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { FrameworkConfiguration } from 'aurelia-framework';
 import { AureliaAdalConfig } from './aurelia-adal-config';
 import { AureliaAdalManager } from './aurelia-adal-manager';
 
-export function configure(frameworkConfig: FrameworkConfiguration, config: AureliaAdalConfig) {
-  let aureliaAdal: AureliaAdalManager = frameworkConfig.container.get(AureliaAdalManager);
-  
-  aureliaAdal.configure(config);
+export module AureliaAdal {
+    export function configure(frameworkConfig: FrameworkConfiguration, config: AureliaAdalConfig) {
+    let aureliaAdal: AureliaAdalManager = frameworkConfig.container.get(AureliaAdalManager);
+    
+    aureliaAdal.configure(config);
+    }
+    
+    export class AureliaAdalManager2 extends AureliaAdalManager {}
 }
