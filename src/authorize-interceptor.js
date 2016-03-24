35727e5014759ad6da68e48fcd7366c414020e78
Martijn Hoebergen <martijn.hoebergen@servicebureau.nl>
2016-03-24 15:51:52 +0100

    re-implemented adal logic

---
+++ src/authorize-interceptor.js
@@ -0,0 +1,27 @@
+﻿import inject from 'aurelia-dependency-injection';
+import {AdalManager} from './adal-manager';
+
+@inject(AdalManager)
+export class AuthorizeInterceptor {
+
+  constructor(adalManager) {
+    this.adalManager = adalManager;
+  }
+
+  async request(request) {
+    await this.adalManager.loadTokenForRequest(
+      request.url, 
+      token => request.headers.append('Authorization', 'Bearer ' + token),
+      token => request.headers.set('Authorization', 'Bearer ' + token)
+    );
+
+    return request;
+  }
+
+  responseError(rejection) {
+    let notAuthorized = rejection && rejection.status === 401;
+    this.adalManager.handleRequestFailed(rejection.config.url, notAuthorized);
+    
+    return rejection;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export function configure(config) {
-  config.globalResources('./hello-world');
+import {AdalConfig} from 'adal-config';
+import './auth-filter';
+export {AuthorizeStep} from './authorize-step';
+export {AuthorizeInterceptor} from './authorize-interceptor'
+
+export function configure(aurelia, settings) {
+  aurelia.globalResources('./auth-filter');
+
+  let adalConfig = aurelia.container.get(AdalConfig);
+
+  adalConfig.configure(settings);
 }
